a790d9d3879d209ebaecc6a5ef0c037a403b26eb
Mark Pham <markpham@Marks-iMac.local>
2020-07-28 13:32:23 -0700

    adding prisma, graphql, graphql yoga to handle backend data query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,12 @@
-const Query = {};
+
+//parent
+//args: whichever is being queried
+//context: contains the prisma database provided by graphQL server (createServer.js) after it pulls this from prisma
+//context can also pass in any other requests ('req') the query has, like the HTTP request header
+const Query = {
+	dogs: function(parent, args, ctx, info) {
+		return [{name: "Jesus"}, {name: "Sunny"}];
+	}
+};
 
 module.exports = Query;
